e3d407ddae9942400222ca011236d35a957c8734
unknown <sanninm>
2021-09-21 00:41:22 +0200

    Functions - Generate Random Whole Numbers

---
+++ ex12/src/index.js
@@ -0,0 +1,12 @@
+// Only change code below this line
+
+function randnomInteger (){
+
+return Math.floor(Math.random() * 30);
+
+}
+
+
+randnomInteger ();
+// Only change code above this line 
+module.export = randnomInteger;
