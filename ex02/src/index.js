0b539f755941d71d37577d4080e076232bfcc969
Sanninm <mudzelet.sanin@gmail.com>
2021-09-20 15:10:37 +0200

    Functions with Arguments

---
+++ ex02/src/index.js
@@ -0,0 +1,10 @@
+// Write code below this line
+function functionPrintInfo (parameter1, parameter2){
+    console.log(parameter1, parameter2);
+}
+// Write code aboce this line 
+
+module.exports = functionPrintInfo;
+
+functionPrintInfo("John", 23);
+functionPrintInfo("Michael", 43);
