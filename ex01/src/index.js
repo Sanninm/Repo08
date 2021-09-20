2d77c861a167808c8e010e155049133e9c36d12e
Sanninm <mudzelet.sanin@gmail.com>
2021-09-20 14:48:03 +0200

    Functions

---
+++ ex01/src/index.js
@@ -0,0 +1,16 @@
+
+// Write code below this line
+function reusableFunction (){
+    console.log("Hi Padawan !");
+}
+// Write code aboce this line 
+
+module.exports = reusableFunction;
+
+reusableFunction();
+
+
+
+
+
+
