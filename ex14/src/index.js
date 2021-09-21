e36fa23bc368c1b3dcc6fbbb505a7bf9664c2c97
Sanninm <mudzelet.sanin@gmail.com>
2021-09-21 08:34:35 +0200

    Functions - the parseInt()

---
+++ ex14/src/index.js
@@ -0,0 +1,11 @@
+// Only change code below this line 
+function convertStrToInt (str) {
+ return parseInt(str);
+
+}
+
+console.log("44");
+console.log("27");
+console.log("JeffBezos");
+// Only change code above this line
+module.exports = convertStrToInt;
